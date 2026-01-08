2d24c14b057b1ea054c69881c93252ae214ae971
Joshua Chung <joshuarchung2@gmail.com>
2026-01-08 01:21:14 -0800

    Initial Commit

---
+++ SimFolio/SimFolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimFolioApp.swift
+//  SimFolio
+//
+//  Created by Joshua Chung on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct SimFolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
